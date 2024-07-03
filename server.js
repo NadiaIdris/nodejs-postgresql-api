4976cc911cf34ede34e92c8c1ef77b3d2e25973d
Nadia Idris <nadia@fasterlap.com>
2024-07-03 15:19:37 -0500

    Update server file for production environment

--- server.js
+++ server.js
@@ -4,17 +4,20 @@ const studentRoutes = require("./src/student/routes/routes");
 const authRoute = require("./src/user/routes/routes");
 require("dotenv").config();
 
+let isProduction = process.env.NODE_ENV === 'production';
+let origin = isProduction ? '*' : 'http://localhost:3000';
+
 const app = express();
 const port = 4000;
 // Express middleware to parse request body JSON data
 app.use(express.json());
 // Express middleware to enable CORS
 app.use(cors({
-  origin: 'http://localhost:3000', // Replace with your React app's URL,
-  'Access-Control-Allow-Origin': 'http://localhost:3000'
+  origin: origin, // Replace with your React app's URL,
+  'Access-Control-Allow-Origin': origin
 }));
 
-app.use(express.static('../client/build'))
+app.use(express.static('../ts-student-list-api/build'))
 
 app.use('/api/v1/user', authRoute)
 app.use("/api/v1/students", studentRoutes);
