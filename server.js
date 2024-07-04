410cae2e3eda5a0ab5a5056ff13abf25cc7e75c5
Nadia Idris <nadia@fasterlap.com>
2024-07-04 14:30:25 -0500

    Simplify CORS and unify dev and prod configs

--- server.js
+++ server.js
@@ -1,25 +1,24 @@
 const express = require("express");
-const cors = require('cors');
+const cors = require("cors");
 const studentRoutes = require("./src/student/routes/routes");
 const authRoute = require("./src/user/routes/routes");
 require("dotenv").config();
 
-let isProduction = process.env.NODE_ENV === 'production';
-let origin = isProduction ? '*' : 'http://localhost:3000';
+let origin = "*";
 
 const app = express();
 const port = 4000;
 // Express middleware to parse request body JSON data
 app.use(express.json());
 // Express middleware to enable CORS
-app.use(cors({
-  origin: origin, // Replace with your React app's URL,
-  'Access-Control-Allow-Origin': origin
-}));
+app.use(
+  cors({
+    origin: origin, // Replace with your React app's URL,
+    "Access-Control-Allow-Origin": origin,
+  })
+);
 
-app.use(express.static('../ts-student-list-api/build'))
-
-app.use('/api/v1/user', authRoute)
+app.use("/api/v1/user", authRoute);
 app.use("/api/v1/students", studentRoutes);
 
 app.listen(port, () => {
